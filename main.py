b79a62e592015a39eaa444f47e60f149a5654c8b
pminervini <p.minervini@gmail.com>
2021-06-05 17:23:22 +0200

    model.train_step -> KGReasoning.train_step (since it is static)

--- main.py
+++ main.py
@@ -430,14 +430,14 @@ def main(args):
             if step == 2*args.max_steps//3:
                 args.valid_steps *= 4
 
-            log = model.train_step(model, optimizer, train_path_iterator, args, step)
+            log = KGReasoning.train_step(model, optimizer, train_path_iterator, args, step)
             for metric in log:
                 writer.add_scalar('path_'+metric, log[metric], step)
             if train_other_iterator is not None:
-                log = model.train_step(model, optimizer, train_other_iterator, args, step)
+                log = KGReasoning.train_step(model, optimizer, train_other_iterator, args, step)
                 for metric in log:
                     writer.add_scalar('other_'+metric, log[metric], step)
-                log = model.train_step(model, optimizer, train_path_iterator, args, step)
+                log = KGReasoning.train_step(model, optimizer, train_path_iterator, args, step)
 
             training_logs.append(log)
 
